967db4031c3337460d2d5b0dcfa99027bbc30a6b
warmjoker <874307313@qq.com>
2018-05-16 14:07:31 +0800

    Initial Commit

---
+++ TheNextDayTests/TheNextDayTests.swift
@@ -0,0 +1,36 @@
+//
+//  TheNextDayTests.swift
+//  TheNextDayTests
+//
+//  Created by 潘俊舟 on 2018/5/16.
+//  Copyright © 2018年 潘俊舟. All rights reserved.
+//
+
+import XCTest
+@testable import TheNextDay
+
+class TheNextDayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
